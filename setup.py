1c67125f11ea080cbd48c1aff5c9aa5fe242359b
Tom Orth <torth212@gmail.com>
2018-05-31 22:28:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(name='priorityscheduler',
-      version='0.1',
+      version='0.2',
       description='Priority Queue to schedule custom jobs',
       url='https://github.com/TomOrth/Priority-Queue-Job-Scheduler',
-      download_url='https://github.com/atf1999/executioner/tarball/0.1',
+      download_url='https://github.com/atf1999/executioner/tarball/0.2',
       author='Tom Orth',
       author_email='torth212@gmail.com',
       license='MIT',
